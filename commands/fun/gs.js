c7b22fce233a3decf33dd8db5388023a94ce7d40
RedDusty <61934784+RedDusty@users.noreply.github.com>
2022-06-06 08:59:14 +0000

    search by id

--- commands/fun/gs.js
+++ commands/fun/gs.js
@@ -1,41 +1,89 @@
-const { CommandInteraction, MessageEmbed } = require('discord.js');
-const Client = require('../../index').Client;
-const { sysColor } = require('../../angImg');
-const firebase = require('firebase/app');
-require('firebase/firestore');
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+const Client = require("../../index").Client;
+const { sysColor } = require("../../angImg");
+const firebase = require("firebase/app");
+require("firebase/firestore");
 
 /**
  * @param {CommandInteraction} inter
  */
 module.exports.run = async (inter) => {
-  const interString = inter.options.getString('тег');
+  const interString = inter.options.getString("тег");
   if (!interString) {
-    return await inter.reply({ content: 'Введите тег', ephemeral: true });
+    return await inter.reply({
+      content: "Введите тег или id#<число>",
+      ephemeral: true,
+    });
   }
-  const searchString = interString.substr(0, 1).toUpperCase() + interString.substr(1);
-  const resultDoc = await firebase.firestore().collection('albums').where('name', '==', searchString).limit(1).get();
+
+  let resultDoc;
+  let card;
+
+  if (interString.startsWith("id#")) {
+    const id = interString.substr(3);
+    resultDoc = await firebase
+      .firestore()
+      .collection("cards")
+      .where("id", "==", id)
+      .limit(1)
+      .get();
+  } else {
+    resultDoc = await firebase
+      .firestore()
+      .collection("albums")
+      .where("name", "==", searchString)
+      .limit(1)
+      .get();
+  }
+
+  const searchString =
+    interString.substr(0, 1).toUpperCase() + interString.substr(1);
 
   if (resultDoc.empty) {
-    return await inter.reply({ content: 'Ничего не найдено.', ephemeral: true });
+    return await inter.reply({
+      content: "Ничего не найдено.",
+      ephemeral: true,
+    });
   }
-  const cardsIdArray = resultDoc.docs[0].data().cardsId;
 
-  const cardId = cardsIdArray[Math.floor(Math.random() * (cardsIdArray.length - 0) + 0)];
+  let cardInfo;
 
-  const card = await firebase.firestore().collection('cards').where('id', '==', cardId).limit(1).get();
+  if (interString.startsWith("id#")) {
+    cardInfo = card.docs[0].data();
+  } else {
+    const cardsIdArray = resultDoc.docs[0].data().cardsId;
+    const cardId =
+      cardsIdArray[Math.floor(Math.random() * (cardsIdArray.length - 0) + 0)];
+    card = await firebase
+      .firestore()
+      .collection("cards")
+      .where("id", "==", cardId)
+      .limit(1)
+      .get();
+    cardInfo = card.docs[0].data();
+  }
 
-  const cardInfo = card.docs[0].data();
+  if (cardInfo.fileURL && cardInfo.id) {
+    const embed = new MessageEmbed()
+      .setImage(cardInfo.fileURL)
+      .setColor("#9B59B6")
+      .setFooter("os:/fun/gs.info", sysColor("red"))
+      .addField("https://goodsearch.vercel.app/card/" + cardInfo.id, "\u200b");
 
-  const embed = new MessageEmbed()
-    .setImage(cardInfo.fileURL)
-    .setColor('#9B59B6')
-    .setFooter('os:/fun/gs.info', sysColor('red'))
-    .addField('https://goodsearch.vercel.app/card/' + cardInfo.id, '\u200b');
-
-  return await inter.reply({ content: '\u200b', ephemeral: false, embeds: [embed] });
+    return await inter.reply({
+      content: "\u200b",
+      ephemeral: false,
+      embeds: [embed],
+    });
+  } else {
+    return await inter.reply({
+      content: "Ничего не найдено.",
+      ephemeral: true,
+    });
+  }
 };
 
 module.exports.help = {
-  name: 'gs',
-  permission: []
+  name: "gs",
+  permission: [],
 };
